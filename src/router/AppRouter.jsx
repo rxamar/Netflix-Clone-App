8466ec32c1b2308808fef830b53c9419ec59b273
unknown <rohitamar331@gmail.com>
2023-11-30 20:00:46 +0530

    row list

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,40 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Card from "../components/card/Card";
 import SigninPage from "../pages/auth/signIn/SignIn";
 import SignupPage from "../pages/auth/signUp/SignUp";
-<<<<<<< HEAD
-import { getFetchTrending } from "../api/movies";
+import LandingPage from "../pages/public/landingPage/LandingPage";
 import HomePage from "../pages/portal/home/page";
+// import { getFetchTrending } from "../api/movies";
 const AppRouter = () => {
-  getFetchTrending();
+  // getFetchTrending();
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<></>} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/card" element={<Card />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route />
       </Routes>
     </BrowserRouter>
   );
-=======
-import LandingPage from "../pages/public/landingPage/LandingPage";
-// import { getFetchTrending } from "../api/movies";
-const AppRouter = () => {
-	// getFetchTrending();
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<LandingPage />} />
-				<Route path="/signin" element={<SigninPage />} />
-				<Route path="/signup" element={<SignupPage />} />
-				<Route path="/card" element={<Card />} />
-				<Route path="/signup" element={<SignupPage />} />
-				<Route />
-			</Routes>
-		</BrowserRouter>
-	);
->>>>>>> bb54d8e570c4614d7af7f7d442392f4a8e290600
 };
 
 export default AppRouter;
