47731d3bf9769efb34410744d8c8f1b0ef2e068a
unknown <rohitamar331@gmail.com>
2023-11-30 16:30:39 +0530

    api created for movie

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,81 +1,68 @@
-// import "./form.css";
-// const Form = ({ type }) => {
-//   return (
-//     <div>
-//       <input type={type} id="email address" />
-//       <label for="email address">Email address</label>
-//       <br />
-//       <br />
-//     </div>
-//   );
-// };
-
-// export default Form;
-import React, { useState } from "react";
-import "./form.css";
-import Button from "../button/Button";
+import React from "react";
 import Input from "../input/Input";
-
-const SignIn = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  // const handleSignIn = () => {
-  //   // Implement sign-in logic here
-  //   console.log("Email:", email);
-  //   console.log("Password:", password);
-  // };
-
+import Button from "../button/Button";
+import "./form.css";
+const Form = ({ values, isSignUp, onSubmit, onChange }) => {
+  const { email, password } = values;
   return (
     <div className="signin-container">
-      <div className="shadow">
-        <div className="signin-box">
-          <h2 className="sign-text">Sign In</h2>
-          <div className="input-box">
-            <Input
-              type="email"
-              placeholder="Email address"
-              value={email}
-              onchange={(e) => setEmail(e.target.value)}
-            />
-            <Input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onchange={(e) => setPassword(e.target.value)}
-            />
+      <div className="signin-box">
+        <h2 className="sign-text">Sign In</h2>
+        <div className="input-box">
+          <Input
+            type="email"
+            placeholder="Email address"
+            value={email}
+            onchange={onChange}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onchange={onChange}
+          />
 
-            <div className="button-align">
-              <Button
-                size="x-large"
-                color="white"
-                rounded="rounded"
-                bg="bg-1"
-                value="Sign In"
-              />
-            </div>
-          </div>
-          <div className="checkbox">
-            <div className="remember">
-              <Input type="checkbox" className="check" />
-              <p>Remember me</p>
-            </div>
-            <a href="/">Need help?</a>
-          </div>
-          <div className="new">
-            <p>New to Netflix?</p>
-            <a href="/">Sign up now</a>
-          </div>
-          <div className="para">
-            <p>
-              This page is protected by Google reCAPTCHA to ensure you're not a
-              bot.
-            </p>
+          <div className="button-align">
+            <Button
+              size="x-large"
+              color="white"
+              rounded="rounded"
+              bg="bg-1"
+              value="Sign In"
+            />
           </div>
         </div>
+        <div className="checkbox">
+          <div className="remember">
+            <Input type="checkbox" className="check" />
+            <p>Remember me</p>
+          </div>
+          <a href="/">Need help?</a>
+        </div>
+        {isSignUp ? (
+          <>
+            <div className="new">
+              <p>Already subscribed to netflix?</p>
+              <a href="/">Sign in</a>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="new">
+              <p>New to Netflix?</p>
+              <a href="/">Sign up now</a>
+            </div>
+            <div className="para">
+              <p>
+                This page is protected by Google reCAPTCHA to ensure you're not
+                a bot.
+              </p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
 };
 
-export default SignIn;
+export default Form;
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,7 @@
+import { createContext, useContext, useState } from "react";
+
+const AppContext = () => {
+  return <div>AppContext</div>;
+};
+
+export default AppContext;
