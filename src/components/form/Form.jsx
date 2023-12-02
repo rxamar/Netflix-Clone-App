513b6504363f3c98474b71a15e58d983238a1af6
unknown <sudhar.dss@gmail.com>
2023-12-02 12:58:27 +0530

    auth pages completed

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,58 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../input/Input";
 import Button from "../button/Button";
 import "./form.css";
 import { Link } from "react-router-dom";
-const Form = ({ values, isSignUp, onSubmit, onChange }) => {
-	const { email, password } = values;
+const Form = ({ isSignUp }) => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	};
+	// const { email, password } = values;
 	return (
 		<div className="signin-container">
 			<div className="signin-box">
-				<h2 className="sign-text">Sign In</h2>
-				<div className="input-box">
-					<Input
-						type="email"
-						placeholder="Email address"
-						value={email}
-						onchange={onChange}
-					/>
-					<Input
-						type="password"
-						placeholder="Password"
-						value={password}
-						onchange={onChange}
-					/>
-
-<<<<<<< HEAD
-					<div className="button-align">
-						<Button
-							size="x-large"
-							color="white"
-							rounded="rounded"
-							bg="bg-1"
-							value="Sign In"
+				<h2 className="sign-text">{isSignUp ? "Sign Up" : "Sign In"}</h2>
+				<form onSubmit={handleSubmit}>
+					<div className="input-box">
+						<Input
+							name="email"
+							type="email"
+							label="Email address"
+							value={email}
+							onchange={(event) => setEmail(event.target.value)}
 						/>
+						<Input
+							name="password"
+							type="password"
+							label="Password"
+							value={password}
+							onchange={(event) => setPassword(event.target.value)}
+						/>
+
+						<div className="button-align">
+							<Button
+								size="x-large"
+								color="white"
+								rounded="rounded"
+								bg="bg-1"
+								value={isSignUp ? "Sign Up" : "Sign In"}
+							/>
+						</div>
 					</div>
-				</div>
+				</form>
 				<div className="checkbox">
 					<div className="remember">
 						<Input type="checkbox" className="check" />
 						<p>Remember me</p>
 					</div>
-					<a href="/">Need help?</a>
+					{/* <a href="/">Need help?</a> */}
 				</div>
 				{isSignUp ? (
 					<>
 						<div className="new">
 							<p>Already subscribed to netflix?</p>
-							<a href="/">Sign in</a>
+							<Link to="/signin">Sign in</Link>
 						</div>
 					</>
 				) : (
 					<>
 						<div className="new">
 							<p>New to Netflix?</p>
-							<a href="/">Sign up now</a>
+							<Link to="/signup">Sign up now</Link>
 						</div>
 						<div className="para">
 							<p>
@@ -65,49 +74,6 @@ const Form = ({ values, isSignUp, onSubmit, onChange }) => {
 			</div>
 		</div>
 	);
-=======
-          <div className="button-align">
-            <Button
-              size="x-large"
-              color="white"
-              rounded="rounded"
-              bg="bg-1"
-              value="Sign In"
-            />
-          </div>
-        </div>
-        <div className="checkbox">
-          <div className="remember">
-            <Input type="checkbox" className="check" />
-            <p>Remember me</p>
-          </div>
-          <a href="/">Need help?</a>
-        </div>
-        {isSignUp ? (
-          <>
-            <div className="new">
-              <p>Already subscribed to netflix?</p>
-              <Link to="/signin">Sign in</Link>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="new">
-              <p>New to Netflix?</p>
-              <a href="/">Sign up now</a>
-            </div>
-            <div className="para">
-              <p>
-                This page is protected by Google reCAPTCHA to ensure you're not
-                a bot.
-              </p>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
-  );
->>>>>>> ceebbf96b43d91f2b60c440e43e558584f641799
 };
 
 export default Form;
--- src/pages/auth/signUp/SignUp.jsx
+++ src/pages/auth/signUp/SignUp.jsx
@@ -1,29 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Form from "../../../components/form/Form";
-import "./signUp.css";
-import { getFetchTrending } from "../../../api/movies";
-const SignUp = () => {
-	const [user, setUser] = useState({
-		email: "",
-		password: "",
-	});
-	const handleSubmit = () => {};
-	const handleChange = () => {};
-	// const handleSignIn = () => {
-	//   // Implement sign-in logic here
-	//   console.log("Email:", email);
-	//   console.log("Password:", password);
-	// };
-	getFetchTrending();
 
+const SignUp = () => {
 	return (
-		<Form
-			values={user}
-			isSignUp
-			onChange={handleChange}
-			onSubmit={handleSubmit}
-		/>
+		<div className="auth-page">
+			<Form isSignUp />
+		</div>
 	);
 };
-
 export default SignUp;
