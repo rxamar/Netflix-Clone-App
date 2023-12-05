e674ca1c16a30ee3e3c96f58e36e7d4a49aa2be5
unknown <rohitamar331@gmail.com>
2023-12-05 15:44:49 +0530

    changes completes

--- src/pages/portal/search/Page.jsx
+++ src/pages/portal/search/Page.jsx
@@ -11,89 +11,89 @@ import { getPageData } from "../../../api/movies";
 // import Row from "../../../components/row/Row";
 import List from "../../../components/row/List";
 const Page = () => {
-	const [data, setData] = useState(null); // to fetch trending Movies
-	useEffect(() => {
-		getPageData().then((res) => setData(res));
-	}, []);
+  const [data, setData] = useState(null); // to fetch trending Movies
+  useEffect(() => {
+    getPageData().then((res) => setData(res));
+  }, []);
 
-	const [search, setSearch] = useState("");
-	const [movies, setMovies] = useState([]);
-	// const [isSearching, setIsSearching] = useState(false);
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+  // const [isSearching, setIsSearching] = useState(false);
 
-	const handleChange = (event) => {
-		setSearch(event.target.value);
-	};
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
 
-	const getMovies = async (query) => {
-		try {
-			const response = await axios.get(
-				`search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}`
-			);
-			const results = response.data.results;
-			// console.log(response);
-			return results;
-		} catch (err) {
-			console.log(err);
-			return [];
-		}
-	};
+  const getMovies = async (query) => {
+    try {
+      const response = await axios.get(
+        `search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}`
+      );
+      const results = response.data.results;
+      // console.log(response);
+      return results;
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
+  };
 
-	useEffect(() => {
-		if (search) {
-			getMovies(search).then((results) => {
-				setMovies(results);
-			});
-		}
-	}, [search]);
+  useEffect(() => {
+    if (search) {
+      getMovies(search).then((results) => {
+        setMovies(results);
+      });
+    }
+  }, [search]);
 
-	useEffect(() => {
-		console.log(movies);
-	}, [movies]);
+  useEffect(() => {
+    console.log(movies);
+  }, [movies]);
 
-	return (
-		<>
-			<div className="">
-				<Navbar isUser />
-				<div className="search-input">
-					<div className="input">
-						<Input
-							type="search"
-							bg="search"
-							placeholder="Search Movies"
-							value={search}
-							onChange={handleChange}
-						/>
-					</div>
-					<Button
-						color="white"
-						value="Search"
-						size="small"
-						bg="bg-1"
-						rounded="rounded"
-					/>
-				</div>
+  return (
+    <>
+      <div className="">
+        <Navbar isUser />
+        <div className="search-input">
+          <div className="input">
+            <Input
+              type="search"
+              bg="search"
+              placeholder="Search Movies"
+              value={search}
+              onChange={handleChange}
+            />
+          </div>
+          <Button
+            color="white"
+            value="Search"
+            size="small"
+            bg="bg-1"
+            rounded="rounded"
+          />
+        </div>
 
-				<div className="wish-list">
-					{search ? (
-						<div className="result-padding">
-							<h1 className="result-heading">Results for {search}</h1>
-							<div className="wish-list-items">
-								{movies?.map((item) => (
-									<Card movie={item} />
-								))}
-							</div>
-						</div>
-					) : (
-						<>
-							<List title="Trending Now" data={data?.trendingNow.results} />
-						</>
-					)}
-				</div>
-			</div>
-		</>
-	);
+        <div className="wish-list">
+          {search ? (
+            <div className="result-padding">
+              <h1 className="result-heading">Results for {search}</h1>
+              <div className="wish-list-items">
+                {movies?.map((item) => (
+                  <Card movie={item} />
+                ))}
+              </div>
+            </div>
+          ) : (
+            <>
+              <List title="Trending Now" data={data?.trendingNow.results} />
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
-// search page end
+// search page ends
 
 export default Page;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,68 +6,68 @@ import SearchPage from "../pages/portal/search/page";
 import MyListPage from "../pages/portal/myList/page";
 import LandingPage from "../pages/public/landingPage/page";
 import {
-	ProtectedRoute,
-	ProtectedRouteForUnAuthorizedPage,
+  ProtectedRoute,
+  ProtectedRouteForUnAuthorizedPage,
 } from "./ProtectedRoute";
 // import { getFetchTrending } from "../api/movies";
 const AppRouter = () => {
-	// getFetchTrending();
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<ProtectedRouteForUnAuthorizedPage>
-							<LandingPage />
-						</ProtectedRouteForUnAuthorizedPage>
-					}
-				/>
-				<Route
-					path="/signin"
-					element={
-						<ProtectedRouteForUnAuthorizedPage>
-							<SigninPage />
-						</ProtectedRouteForUnAuthorizedPage>
-					}
-				/>
-				<Route
-					path="/signup"
-					element={
-						<ProtectedRouteForUnAuthorizedPage>
-							<SignupPage />
-						</ProtectedRouteForUnAuthorizedPage>
-					}
-				/>
-				<Route
-					path="/home"
-					element={
-						<ProtectedRoute>
-							<HomePage />
-						</ProtectedRoute>
-					}
-				/>
-				<Route
-					path="/mylist"
-					element={
-						<ProtectedRoute>
-							<MyListPage />
-						</ProtectedRoute>
-					}
-				/>
-				<Route
-					path="/search"
-					element={
-						<ProtectedRoute>
-							<SearchPage />
-						</ProtectedRoute>
-					}
-				/>
-				<Route />
-			</Routes>
-		</BrowserRouter>
-		// route end
-	);
+  // getFetchTrending();
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRouteForUnAuthorizedPage>
+              <LandingPage />
+            </ProtectedRouteForUnAuthorizedPage>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <ProtectedRouteForUnAuthorizedPage>
+              <SigninPage />
+            </ProtectedRouteForUnAuthorizedPage>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <ProtectedRouteForUnAuthorizedPage>
+              <SignupPage />
+            </ProtectedRouteForUnAuthorizedPage>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/mylist"
+          element={
+            <ProtectedRoute>
+              <MyListPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <ProtectedRoute>
+              <SearchPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+    // route end
+  );
 };
 
 export default AppRouter;
