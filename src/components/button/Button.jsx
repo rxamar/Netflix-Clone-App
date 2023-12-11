95dfa356758af8f39d931b2853ffbeb199d3345b
unknown <rohitamar331@gmail.com>
2023-12-11 14:56:31 +0530

    clear console

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import "./button.css";
 
 const Button = ({
-	bg,
-	size,
-	color,
-	value,
-	rounded,
-	icon,
-	onclick,
-	iconProp,
-	type,
+  bg,
+  size,
+  color,
+  value,
+  rounded,
+  icon,
+  onclick,
+  iconProp,
+  type,
 }) => {
-	return (
-		<button
-			className={`${bg || ""} ${color || ""} ${rounded && rounded} ${
-				size || ""
-			} `}
-			type={type || ""}
-			onClick={onclick}
-		>
-			{value}
-			{icon && <div className={`${iconProp || ""}`}>{icon}</div>}
-		</button>
-	);
+  return (
+    <button
+      className={`${bg || ""} ${color || ""} ${rounded && rounded} ${
+        size || ""
+      } `}
+      type={type || ""}
+      onClick={onclick}
+    >
+      {value}
+      {icon && <div className={`${iconProp || ""}`}>{icon}</div>}
+    </button>
+  );
 };
 
 export default Button;
--- src/pages/auth/signIn/page.jsx
+++ src/pages/auth/signIn/page.jsx
@@ -2,13 +2,15 @@ import "./signIn.css";
 
 import React from "react";
 import Form from "../../../components/form/Form";
+import Footer from "../../../components/footer/Footer";
 
 const SignIn = () => {
-	return (
-		<div className="auth-page">
-			<Form isSignin />
-		</div>
-	);
+  return (
+    <div className="auth-page">
+      <Form isSignin />
+      <Footer />
+    </div>
+  );
 };
 
 export default SignIn;
